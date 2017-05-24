dc5fdbd60423422e1a1562bf351ccc2143f56208
Ignasi Ballarà Franco <iballara@gmail.com>
2017-05-24 21:13:38 +0200

    Primera part del control remot.

--- app/src/main/java/manel/com/manel/activities/RemoteControlActivity.java
+++ app/src/main/java/manel/com/manel/activities/RemoteControlActivity.java
@@ -5,28 +5,54 @@ import android.gesture.GestureLibraries;
 import android.gesture.GestureLibrary;
 import android.gesture.GestureOverlayView;
 import android.gesture.Prediction;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import manel.com.manel.R;
+import manel.com.manel.utils.Constants;
+
+import static manel.com.manel.utils.Constants.RemoteControl.ANGLE_LEFT_1;
+import static manel.com.manel.utils.Constants.RemoteControl.ANGLE_RIGHT_1;
+import static manel.com.manel.utils.Constants.RemoteControl.ANGLE_RIGHT_2;
+import static manel.com.manel.utils.Constants.RemoteControl.ANGLE_ZERO;
+import static manel.com.manel.utils.Constants.RemoteControl.AUTOMATIC_MODE;
+import static manel.com.manel.utils.Constants.RemoteControl.MANUAL_MODE;
+import static manel.com.manel.utils.Constants.RemoteControl.STOPPED;
 
 /**
  * Activty to control the robot. It also receives information from it
  * like temperature or velocity.
  *
  * @author  Ignasi Ballara, Joaquim Porte, Arnau Tienda
- * @version 1.0
+ * @version 2.0
  */
 public class RemoteControlActivity extends AppCompatActivity
-        implements GestureOverlayView.OnGesturePerformedListener{
+        implements GestureOverlayView.OnGesturePerformedListener, SensorEventListener {
 
     private GestureLibrary gestureLib;
+    private Boolean drivingModeManual;
+    private Integer currentGear;
+    private Boolean ligthsOn;
+
+    // Views
+    TextView tvTemperature, tvSpeed;
+    Button btnGearsPlus, btnGearsMinus, btnAutMan, btnLights, btnAccelerate;
+    View bumperLeft, bumperCenter, bumperRight;
 
     /**
      * OnCreate Method from Activity.
@@ -43,12 +69,26 @@ public class RemoteControlActivity extends AppCompatActivity
         gestureOverlayView.addView(inflate);
         gestureOverlayView.addOnGesturePerformedListener(this);
         gestureLib = GestureLibraries.fromRawResource(this, R.raw.gestures);
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL
+        );
         if (!gestureLib.load()) {
             System.out.println("GestureLib not loaded");
             finish();
         }
+        this.drivingModeManual = true;
+        this.currentGear = STOPPED;
+        this.ligthsOn = false;
         setContentView(gestureOverlayView);
+        setViews();
     }
+
+    public static void writeData() {
+
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -76,6 +116,87 @@ public class RemoteControlActivity extends AppCompatActivity
         if (prediction.score > 1.0 && prediction.name.length()>1) {
             Toast.makeText(this, prediction.name , Toast.LENGTH_SHORT).show();
         }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType()==Sensor.TYPE_ACCELEROMETER){
+            double y = event.values[1];
+            int angle;
+            if (y < -6) {
+                angle = Constants.RemoteControl.ANGLE_LEFT_2;
+            } else if (-6 < y && y < -3) {
+                angle = ANGLE_LEFT_1;
+            } else if (-3 < y && y < 3) {
+                angle = ANGLE_ZERO;
+            } else if (3 <  y && y < 6) {
+                angle = ANGLE_RIGHT_1;
+            } else {
+                angle = ANGLE_RIGHT_2;
+            }
+
+            //TODO: enviar al robot.
+
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    private void setViews() {
+
+
+        tvTemperature = (TextView) findViewById(R.id.tv_temperature);
+        btnGearsPlus = (Button) findViewById(R.id.button_gears_minus);
+        btnGearsMinus = (Button) findViewById(R.id.button_gears_minus);
+        tvSpeed = (TextView) findViewById(R.id.tv_velocimeter);
+        btnAutMan = (Button) findViewById(R.id.btn_automatic_manual);
+        btnLights = (Button) findViewById(R.id.btn_light_control);
+        bumperLeft = findViewById(R.id.bumper_left);
+        bumperCenter = findViewById(R.id.bumper_collision);
+        bumperRight = findViewById(R.id.bumper_right);
+        btnAccelerate = (Button) findViewById(R.id.acc_button);
+
+        final List<Button> viewsToDeactivate = Arrays.asList(btnGearsPlus, btnGearsMinus,
+                btnAccelerate, btnLights);
+
+        btnGearsPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentGear++;
+            }
+        });
+
+        btnGearsMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentGear--;
+            }
+        });
+
+        btnAutMan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drivingModeManual = !drivingModeManual;
+
+                if (!drivingModeManual) {
+                    // We deactivate some buttons if we are riding in automatic.
+                    for (Button button : viewsToDeactivate) {
+                        button.setClickable(false);
+                        button.setEnabled(false);
+                    }
+
+                } else {
+                    for (Button button : viewsToDeactivate) {
+                        button.setClickable(true);
+                        button.setEnabled(true);
+                    }
+                }
+            }
+        });
+
 
     }
 }
---
+++ app/src/main/java/manel/com/manel/utils/Constants.java
@@ -0,0 +1,28 @@
+package manel.com.manel.utils;
+
+public class Constants {
+    
+    public static class RemoteControl{
+        public static final int AUTOMATIC_MODE = 0;
+        public static final int MANUAL_MODE = 1;
+
+        // Direction.
+        public static final int STOPPED = 0;
+        public static final int FORWARD = 1;
+        public static final int BACKWARD = -1;
+
+        // Angle detected from accelerometer.
+        public static final int ANGLE_ZERO = 10;
+        public static final int ANGLE_RIGHT_1 = 11;
+        public static final int ANGLE_RIGHT_2 = 12;
+        public static final int ANGLE_LEFT_1 = 21;
+        public static final int ANGLE_LEFT_2 = 22;
+
+        // 
+        public static final int SQUARE = 30;
+        public static final int TRIANGLE = 31;
+        public static final int CIRCLE = 31;
+    }
+
+
+}
